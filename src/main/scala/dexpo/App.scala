4e2af19cf6cb38619f05dc0d7dd16bbfc4cf5610
sidnt <sidnt@users.noreply.github.com>
2020-10-06 09:03:44 +0530

    [fail] timer working on android but not web

--- src/main/scala/dexpo/App.scala
+++ src/main/scala/dexpo/App.scala
@@ -5,10 +5,54 @@ import slinky.native._
 import slinky.core.annotations.react
 import slinky.core.FunctionalComponent
 import slinky.core._
+import facade.Hooks._
 
 @react object App {
+
+  /**
+    * if we keep `App` as the top/root level component/app
+    * handed off to expo machinery
+    * is it possible that we are able to run a zio subsystem
+    * inside this component, at its top level?
+    * sure we could run arbitrary code
+    * and have it manange the state related stuff?
+    * .
+    * App would be handed off to expo
+    * it would be loaded
+    * zio subsytem would be kicked off into action
+    * it would manage some external effects, eg network calls.
+    * (don't yet know about user interaction changing state)
+    */
+  var state = 0
+  import zio._
+  import duration._
+  val perSecond = Schedule.spaced(1.second)
+  val zrts = Runtime.default
+  val mutator =
+    for {
+      _ <- UIO(state += 1).repeat(perSecond)
+    } yield ()
+  
+
+  /**
+    * problem here:
+    * [1] on android, the timer runs so long as the app is in focus
+    * navigate to other app OR turn the screen off, the Schedule suspends
+    * [2] on web: the error goes :
+    * `java.time.zone.ZoneRulesException: Unknown time-zone ID: Asia/Calcutta`
+    */
+  val run = zrts.unsafeRunAsync_(mutator)
+
   val component = FunctionalComponent[Unit] { _ =>
-    View(style = centerStyle)(Text("ℍ\uD835\uDD56\uD835\uDD5D\uD835\uDD5D\uD835\uDD60. \uD835\uDD3D\uD835\uDD63\uD835\uDD60\uD835\uDD5E \uD835\uDD52 \uD835\uDD3D\uD835\uDD66\uD835\uDD5F\uD835\uDD54\uD835\uDD65\uD835\uDD5A\uD835\uDD60\uD835\uDD5F\uD835\uDD52\uD835\uDD5D ℂ\uD835\uDD60\uD835\uDD5E\uD835\uDD61\uD835\uDD60\uD835\uDD5F\uD835\uDD56\uD835\uDD5F\uD835\uDD65."))
+    val (count, setCount) = useState(0)
+    View(style = centerStyle)(
+      Text("ℍ\uD835\uDD56\uD835\uDD5D\uD835\uDD5D\uD835\uDD60. \uD835\uDD3D\uD835\uDD63\uD835\uDD60\uD835\uDD5E \uD835\uDD52 \uD835\uDD3D\uD835\uDD66\uD835\uDD5F\uD835\uDD54\uD835\uDD65\uD835\uDD5A\uD835\uDD60\uD835\uDD5F\uD835\uDD52\uD835\uDD5D ℂ\uD835\uDD60\uD835\uDD5E\uD835\uDD61\uD835\uDD60\uD835\uDD5F\uD835\uDD56\uD835\uDD5F\uD835\uDD65."),
+      Button(
+        title = "Refresh Seconds Elapsed",
+        onPress = () => setCount(state)
+      ),
+      Text(s"seconds elapsed: $count")
+    )
   }
 }
 
